53d2a1d2f62681028ed0364ff39c624c48ea10de
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2024-04-13 18:07:10 +0100

    partner update

--- src/components/PartnerPic.jsx
+++ src/components/PartnerPic.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import path1 from '../assests/path1.jpg';
+import law1 from '../assests/law1.jpg';
+import law2 from '../assests/law2.jpg';
+import law3 from '../assests/law3.jpg';
+import law4 from '../assests/law4.jpg';
+import law5 from '../assests/law5.jpg';
+import law6 from '../assests/law6.jpg';
+import law7 from '../assests/law7.jpg';
 
 const PartnerPic = () => {
   return (
@@ -7,33 +14,93 @@ const PartnerPic = () => {
       <div class='flex justify-around flex-col md:flex-row items-center'>
         <div class='flex flex-col justify-center items-center m-4'>
           <div class='relative'>
-            <img src={path1} alt='partner' class=' w-64 h-auto rounded-lg' />
+            <img src={law1} alt='partner' class=' w-64 h-auto rounded-lg' />
             <div class='absolute top-48 left-0 text-white p-4'>
-              <h2 class='text-lg font-bold mb-2'>Ifeoluwa Oladeni</h2>
-              <p>Criminal Lawyer</p>
-              <p>law123</p>
+              <h2 class='text-lg font-bold mb-2'>IKOKWU CHINEKWU WINIFRED</h2>
+              <p>SCN138613</p>
+              <p>2022</p>
             </div>
           </div>
         </div>
 
         <div class='flex flex-col justify-center items-center m-4'>
           <div class='relative'>
-            <img src={path1} alt=' partner1' class=' w-64 h-auto rounded-lg' />
+            <img src={law2} alt='partner' class=' w-64 h-auto rounded-lg' />
             <div class='absolute top-48 left-0 text-white p-4'>
-              <h2 class='text-lg font-bold mb-2'>Ifeoluwa Oladeni</h2>
-              <p>Criminal Lawyer</p>
-              <p>law123</p>
+              <h2 class='text-lg font-bold mb-2'>AKPAN, UNYIME JOSEPH</h2>
+              <p>SCN102262</p>
+              <p>2015</p>
             </div>
           </div>
         </div>
 
         <div class='flex flex-col justify-center items-center m-4'>
           <div class='relative'>
-            <img src={path1} alt='partner1' class=' w-64 h-auto rounded-lg' />
+            <img src={law3} alt=' partner1' class=' w-64 h-auto rounded-lg' />
             <div class='absolute top-48 left-0 text-white p-4'>
-              <h2 class='text-lg font-bold mb-2'>Ifeoluwa Oladeni</h2>
-              <p>Criminal Lawyer</p>
-              <p>law123</p>
+              <h2 class='text-lg font-bold mb-2'>DAVID, CHIDUBEM BAKWUNYE</h2>
+              <p>SCN094376</p>
+              <p>2014</p>
+            </div>
+          </div>
+        </div>
+
+        <div class='flex flex-col justify-center items-center m-4'>
+          <div class='relative'>
+            <img src={law5} alt='partner1' class=' w-64 h-auto rounded-lg' />
+            <div class='absolute top-48 left-0 text-white p-4'>
+              <h2 class='text-lg font-bold mb-2'>APUTAZIE, CHINYERE LAURA</h2>
+              <p>SCN131546</p>
+              <p>2021</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      {/* flex2 */}
+      <div class='flex justify-around flex-col md:flex-row items-center'>
+        <div class='flex flex-col justify-center items-center m-4'>
+          <div class='relative'>
+            <img src={law6} alt='partner' class=' w-64 h-auto rounded-lg' />
+            <div class='absolute top-48 left-0 text-black p-4'>
+              <h2 class='text-lg font-bold mb-2'>OGIDO HALIMAT SADIYA</h2>
+              <p>SCN138843</p>
+              <p>2022</p>
+            </div>
+          </div>
+        </div>
+
+        <div class='flex flex-col justify-center items-center m-4'>
+          <div class='relative'>
+            <img src={law7} alt='partner' class=' w-64 h-auto rounded-lg' />
+            <div class='absolute top-48 left-0 text-white p-4'>
+              <h2 class='text-lg font-bold mb-2'>
+                OLALEYE, BODUNDE OLUWATOYIN
+              </h2>
+              <p>SCN098945</p>
+              <p>2015</p>
+            </div>
+          </div>
+        </div>
+
+        <div class='flex flex-col justify-center items-center m-4'>
+          <div class='relative'>
+            <img src={law3} alt=' partner1' class=' w-64 h-auto rounded-lg' />
+            <div class='absolute top-48 left-0 text-white p-4'>
+              <h2 class='text-lg font-bold mb-2'>DAVID, CHIDUBEM BAKWUNYE</h2>
+              <p>SCN094376</p>
+              <p>2014</p>
+            </div>
+          </div>
+        </div>
+
+        <div class='flex flex-col justify-center items-center m-4'>
+          <div class='relative'>
+            <img src={law5} alt='partner1' class=' w-64 h-auto rounded-lg' />
+            <div class='absolute top-48 left-0 text-white p-4'>
+              <h2 class='text-lg font-bold mb-2'>APUTAZIE, CHINYERE LAURA</h2>
+              <p>SCN131546</p>
+              <p>2021</p>
             </div>
           </div>
         </div>
