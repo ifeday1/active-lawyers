5b9cecba2d0af3cee772e48fdebf20cde7a7b5fc
ifeoluwa oladeni <oladeniifeoluwa123@gmail.com>
2024-04-13 17:00:52 +0100

    fix

--- src/components/PartnerPic.jsx
+++ src/components/PartnerPic.jsx
@@ -7,7 +7,7 @@ const PartnerPic = () => {
       <div class='flex justify-around flex-col md:flex-row items-center'>
         <div class='flex flex-col justify-center items-center m-4'>
           <div class='relative'>
-            <img src={path1} alt='Image 1' class=' w-64 h-auto rounded-lg' />
+            <img src={path1} alt='partner' class=' w-64 h-auto rounded-lg' />
             <div class='absolute top-48 left-0 text-white p-4'>
               <h2 class='text-lg font-bold mb-2'>Ifeoluwa Oladeni</h2>
               <p>Criminal Lawyer</p>
@@ -18,7 +18,7 @@ const PartnerPic = () => {
 
         <div class='flex flex-col justify-center items-center m-4'>
           <div class='relative'>
-            <img src={path1} alt='Image 1' class=' w-64 h-auto rounded-lg' />
+            <img src={path1} alt=' partner1' class=' w-64 h-auto rounded-lg' />
             <div class='absolute top-48 left-0 text-white p-4'>
               <h2 class='text-lg font-bold mb-2'>Ifeoluwa Oladeni</h2>
               <p>Criminal Lawyer</p>
@@ -29,7 +29,7 @@ const PartnerPic = () => {
 
         <div class='flex flex-col justify-center items-center m-4'>
           <div class='relative'>
-            <img src={path1} alt='Image 1' class=' w-64 h-auto rounded-lg' />
+            <img src={path1} alt='partner1' class=' w-64 h-auto rounded-lg' />
             <div class='absolute top-48 left-0 text-white p-4'>
               <h2 class='text-lg font-bold mb-2'>Ifeoluwa Oladeni</h2>
               <p>Criminal Lawyer</p>
